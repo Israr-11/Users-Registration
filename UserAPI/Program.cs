dd762a43606711467c0b228e7f9c7a128fb97dea
Israr Ahmed <91403838+Israr-11@users.noreply.github.com>
2025-01-02 17:37:02 +0000

    Updating the User Registration

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.EntityFrameworkCore;
 using UserAPI.Models;
 
+//Creating the builder for the web application
 var builder = WebApplication.CreateBuilder(args);
 
 // Fetch connection string from appsettings.json
